34fef968c46f4b102285cc0cd854561b0ada970c
Shashikant <shashikantprajapti17@gmail.om>
2024-07-22 11:50:13 +0530

    ...

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PORT = 5000;
+const PORT = 50000;
 const MONGODB_URI =
   "mongodb+srv://shashikant2002:4886Shashi%40384443@cluster0.f9tcemk.mongodb.net/Url_Shortner?retryWrites=true&w=majority";
 const WEB_TITLE = "URL Shortner !!";
